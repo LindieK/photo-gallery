70e6eb18257c56a78486686bfc538b173fb6c9ec
Linda Okorie <lindaokorie27@gmail.com>
2021-01-28 01:23:43 +0100

    separated styled components

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,19 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+import {GridBox} from '../styles/GridBoxStyles'
 import LazyImage from './LazyImage'
-
-const GridBox = styled.div`
-    position: relative;
-    top: 139px;
-    width:70vw;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 1em;
-    padding: 1em;
-    margin: 0 auto;
-
-
-`;
 
 
 export default function Grid() {
