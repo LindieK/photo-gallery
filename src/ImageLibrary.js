7a9ad95c5427f3f65fc3713b00ba846efececb65
Linda Okorie <lindaokorie27@gmail.com>
2021-01-30 00:41:58 +0100

    created dummy image library

---
+++ src/ImageLibrary.js
@@ -0,0 +1,51 @@
+export const library = [
+    {
+        id: "1",
+        src: "./dummy-images/mag1.jpg"
+    },
+
+    {
+        id:"2",
+        src:"./dummy-images/mag2.jpg"
+    },
+
+    {
+        id:"3",
+        src:"./dummy-images/mag4b.jpg"
+    },
+
+    {
+        id:"4",
+        src:"./dummy-images/mag5.jpg"
+    },
+
+    {
+        id:"5",
+        src:"./dummy-images/mag6.jpg"
+    },
+
+    {
+        id:"6",
+        src:"./dummy-images/mag9.jpg"
+    },
+
+    {
+        id:"7",
+        src:"./dummy-images/mag10.jpg"
+    },
+
+    {
+        id:"8",
+        src:"./dummy-images/mag11.jpg"
+    },
+
+    {
+        id:"9",
+        src:"./dummy-images/mag13.jpg"
+    },
+
+    {
+        id:"10",
+        src:"./dummy-images/mag14a.jpg"
+    }
+]
