bf746b13a7ab77ff0d437322176ed86299e20bd7
Linda Okorie <lindaokorie27@gmail.com>
2021-01-30 20:48:00 +0100

    deleted empty image component

--- src/components/Image.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {StyledImage} from '../styles/ImagesStyles'
-
-export default function Image(props) {
-    return (
-        <StyledImage  src={props.image}/>
-    )
-}
