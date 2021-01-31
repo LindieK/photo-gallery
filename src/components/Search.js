02d7912f649e382402b020385620406e3a9e7a5b
Linda Okorie <lindaokorie27@gmail.com>
2021-01-31 23:30:02 +0100

    added comment on search query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import '../styles/search.css'
 
+
+//search call: https://api.unsplash.com/photos/random/?client_id=1e93497bee7b143fe497bd50d3baaac2536d4d7abfc0fea16090aa38614ad135&count=4&query=dogs
+
 class Search extends Component {
     constructor(props) {
         super(props)
