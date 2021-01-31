0145e4be19402d6dad0a3cb72a8d81a1f5544ae1
Linda Okorie <lindaokorie27@gmail.com>
2021-01-31 23:29:21 +0100

    made fetch requeat

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {GridBox} from '../styles/GridBoxStyles'
 import LazyImage from './LazyImage'
-import {library}from '../ImageLibrary'
+import axios from 'axios'
 
 
+const clientID = "1e93497bee7b143fe497bd50d3baaac2536d4d7abfc0fea16090aa38614ad135";
+const numberOfPhotos = 30;
+const url = "https://api.unsplash.com/photos/random/?client_id=" + clientID + "&count=" + numberOfPhotos;
 
 
-export default function Grid() {
-    return(
-        <GridBox>
-            {library.map(image => (
-                <LazyImage key={image.id} image={`https://picsum.photos/1000/1000?random=${image.id}`} />
-            ))}
-        </GridBox>
-    )
+class Grid extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             photos: []
+        }
+    }
+
+    
+    componentDidMount(){
+        axios.get(url)
+        .then(response => {
+            this.setState({
+                photos: response.data
+            })
+        })
+        .catch()
+    }
+
+    
+    render() {
+        const {photos} = this.state;
+        return (
+            <GridBox>
+                {photos.length ? 
+                photos.map(photo => 
+                    <LazyImage key={photo.id} image={photo.urls.regular} alt={photo.alt_description}/>
+                    ) : 
+                null
+                }
+            </GridBox>
+        )}
 }
+
+export default Grid;
