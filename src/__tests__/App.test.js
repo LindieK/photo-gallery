cf48dafb7e6953e9a4c45a7efc654c9d273015df
Linda Okorie <lindaokorie27@gmail.com>
2021-02-04 22:09:06 +0100

    re-addressed files

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './components/App';
+import Grid from './components/Grid'
+import Nav from './components/Nav'
+
+describe('App Component', () => {
+  it('should render Nav and Grid Components', () => {
+    const app = shallow(<App />);
+
+    expect(app.containsAllMatchingElements([<Nav />,<Grid />])).toEqual(true);
+  });
+  
+});
