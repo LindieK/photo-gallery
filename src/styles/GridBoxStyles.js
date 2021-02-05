e9feb6a23b1a2c376495f166abbb45a1e5d60ecf
Linda Okorie <lindaokorie27@gmail.com>
2021-02-05 12:54:44 +0100

    added styles for positioning on different screens

--- src/styles/GridBoxStyles.js
+++ src/styles/GridBoxStyles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const GridBox = styled.div`
     position: relative;
-    top: 139px;
+    top: 14em;
     width:75vw;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
@@ -12,9 +12,11 @@ export const GridBox = styled.div`
 
     @media only screen and (max-width:767px){
         grid-template-columns: 1fr;
+        top: 10em;
         width: 90vw;
     }
     @media only screen and (min-width:768px) and (max-width: 992px){
         grid-template-columns: 1fr 1fr;
+        top: 12em;
     }
 `;
