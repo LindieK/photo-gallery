cefe2f7bc4f2a9a0175ea2d607e4425d8d25fb07
Linda Okorie <lindaokorie27@gmail.com>
2021-02-05 12:55:03 +0100

    abstracted API calls

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react'
 import {GridBox} from '../styles/GridBoxStyles'
 import LazyImage from './LazyImage'
-import axios from 'axios'
+import {getInitialPics} from '../Api'
 
-
-const clientID = "1e93497bee7b143fe497bd50d3baaac2536d4d7abfc0fea16090aa38614ad135";
-const numberOfPhotos = 30;
-const url = "https://api.unsplash.com/photos/random/?client_id=" + clientID + "&count=" + numberOfPhotos;
 
 
 class Grid extends Component {
@@ -20,13 +16,11 @@ class Grid extends Component {
 
     
     componentDidMount(){
-        axios.get(url)
-        .then(response => {
+        getInitialPics().then(response => {
             this.setState({
                 photos: response.data
             })
         })
-        .catch()
     }
 
     
