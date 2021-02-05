a8c95eb1c01de3969ee5944047138daf618d22be
Linda Okorie <lindaokorie27@gmail.com>
2021-02-05 12:41:37 +0100

    bstracted API calls

---
+++ src/Api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+
+const clientID = "1e93497bee7b143fe497bd50d3baaac2536d4d7abfc0fea16090aa38614ad135";
+const numberOfPhotos = 30;
+let url;
+
+
+export const getSearchResults =  (searchValue) => {
+    url = searchValue ? `https://api.unsplash.com/photos/random/?client_id=${clientID}&count=${numberOfPhotos}&query=${searchValue}`: `https://api.unsplash.com/photos/random/?client_id=${clientID}&count=${numberOfPhotos}` ;
+    return axios.get(url).then(response => response.json())
+};
+
+export const getInitialPics = async () => {
+    url = `https://api.unsplash.com/photos/random/?client_id=${clientID}&count=${numberOfPhotos}`;
+    return axios.get(url)
+    .then(response => {
+        return response
+    });
+}
