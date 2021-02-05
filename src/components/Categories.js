fc396b0119d7ffbb7ac8619e55c8e5ad37ec4621
Linda Okorie <lindaokorie27@gmail.com>
2021-02-05 22:53:56 +0100

    added state change

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react'
 import '../styles/categories.css'
 
 class Categories extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             activeTab: ""
+        }
+    }
+    
+    onTabClick = (tab)=> {
+        this.setState({
+            activeTab: tab
+        })
+    }
+
     render() {
         const Tabs =["For You", "Portraits", "Landscapes", "Architecture", "Profile", "Nature"];
         return (
             <div>
                 <ul>
-                    {Tabs.map(tab => <li>{tab}</li>)}
+                    {Tabs.map(tab => <li key={tab} onClick={this.onTabClick}>{tab}</li>)}
                 </ul>
             </div>
         )
