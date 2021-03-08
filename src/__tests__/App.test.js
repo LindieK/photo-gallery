39f372ab7f9b6f21e1289e33c6a974fdf7bdd74f
Linda Okorie <lindaokorie27@gmail.com>
2021-03-08 17:56:06 +0100

    wrote tests for components

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow} from 'enzyme';
 import App from '../App';
 import Grid from '../components/Grid'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
+import ImageModal from '../components/ImageModal';
+import InfoModal from '../components/InfoModal';
+import Spinner from '../components/Spinner';
 
 describe('App Component', () => {
-  it('should render Nav and Grid Components', () => {
-    const app = shallow(<App />);
+  let app;
 
+  beforeEach(() => {
+    app = shallow(<App />);
+
+  });
+
+  afterEach(() => {
+    app.unmount();
+  })
+
+  it('should render Nav bar', () => {
     expect(app.containsAllMatchingElements([<Search/>, <Categories />])).toEqual(true);
   });
-  
+
+  it('should not render Grid on page entry', () => {
+    expect(app.containsMatchingElement(<Grid />)).toEqual(false);
+  });
+
+  it('should render the Spinner on page entry', () => {
+    expect(app.containsMatchingElement(<Spinner />)).toEqual(true);
+  })
+
+  it('should not render Image Modal', () => {
+    expect(app.containsMatchingElement(<ImageModal />)).toEqual(false);
+  });
+
+  it('should not render Info Modal', () => {
+    expect(app.containsMatchingElement(<InfoModal />)).toEqual(false);
+  });
 });
