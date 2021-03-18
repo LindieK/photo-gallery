b4bb730819b498781da0fe5aa388c589087d6724
Linda Okorie <lindaokorie27@gmail.com>
2021-03-18 01:43:38 +0100

    created profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {CollectionBox, Header, Avatar, UserInfo} from '../styles/ProfileStyles'
 
-export class Profile extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+function Profile(props){
+    return (
+        <div>
+            <Header>
+                <Avatar/>
+                <UserInfo>
+                    <h1>{/*Name of person here*/}</h1>
+                    <button>Edit Profile</button>
+                </UserInfo>
+            </Header>
+            <h2>Collections</h2>
+            <CollectionBox>
+                {/*ImageCollection here*/}
+            </CollectionBox>
+        </div>
+    )
 }
 
 export default Profile
