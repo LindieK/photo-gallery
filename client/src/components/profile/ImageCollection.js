9683579683d2809d1b5aca4e40f3b81ad0ea21f7
Linda Okorie <lindaokorie27@gmail.com>
2021-03-18 01:42:31 +0100

    moved files into new folder

---
+++ client/src/components/profile/ImageCollection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {CollectionWrapper, Placeholder,} from '../../styles/ProfileStyles'
+import LazyLoad from 'react-lazyload'
+
+export default function ImageCollection() {
+    return (
+        <CollectionWrapper>
+            <LazyLoad>
+
+            </LazyLoad>
+        </CollectionWrapper>
+    )
+}
