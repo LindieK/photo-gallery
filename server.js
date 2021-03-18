b221f5d11c9ba036501ee4ff9bf6776e0f94a53a
Linda Okorie <lindaokorie27@gmail.com>
2021-03-18 01:46:43 +0100

    connected to database

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const {MongoClient} = require('mongodb');
+const {MONGOURI} = require('./keys');
+const client = new MongoClient(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(express.json());
+app.use(require('./routes/auth'));
+
+//attempting to make DB connection
+client.connect()
+.then(client => {
+    console.log("MongoDB Database connected...");
+    const db = client.db('photo-gallery-users');
+    const usersCollection = db.collection('users');
+
+    //======== Middleware =========
+
+    //======== Routes ============
+    
+})
+.catch()
+
 
 app.listen(port, () => {
     console.log(`Example app listening at port ${port}`);
