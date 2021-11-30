329ace250882fc8c32844336326a943194c970f3
Linda Okorie <lindaokorie27@gmail.com>
2021-11-30 23:00:24 +0100

    fix: added layout routes for the entire app and auth pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,30 @@
-import { useRoutes } from "react-router";
+import { Navigate, useRoutes } from "react-router";
 
 import Home from "../pages/Home";
-import LoginSignUp from "../pages/LoginSignUp";
+import AuthLayout from "../layouts/AuthLayout";
+import MainLayout from "../layouts/MainLayout";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 import Profile from "../pages/Profile";
 
 const Router = () => {
   let element = useRoutes([
-    { path: "/", element: <LoginSignUp /> },
-    { path: "home", element: <Home /> },
-    { path: "profile", element: <Profile /> },
+    {
+      path: "/",
+      element: <AuthLayout />,
+      children: [
+        { path: "login", element: <Login /> },
+        { path: "signup", element: <SignUp /> },
+        { path: "/", element: <Navigate to="/login" /> },
+      ],
+    },
+    {
+      element: <MainLayout />,
+      children: [
+        { path: "home", element: <Home /> },
+        { path: "profile", element: <Profile /> },
+      ],
+    },
   ]);
 
   return element;
