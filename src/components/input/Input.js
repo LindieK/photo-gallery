2d1da8ed922a9688f06f4a8b62070cbfc7473183
Linda Okorie <lindaokorie27@gmail.com>
2021-12-03 22:13:14 +0100

    fix: added label to component and styled further

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -2,32 +2,50 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const StyledInputContainer = styled.div`
+  margin: 1em 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledInputLabel = styled.label`
+  font-weight: bold;
+  font-size: 0.75em;
+  color: #333333;
+  margin-bottom: 0.5em;
+`;
+
 const StyledInput = styled.input`
   border: 1px solid #9e9ea0;
   border-radius: 8px;
-  padding: 0.5em;
+  padding: 0.75em;
   font-size: 18px;
 
   &:focus {
     border: 2px solid #504cca;
     outline: none !important;
-    background-color: #dcdbf4;
+    background-color: #dfdef9;
   }
 `;
 //TODO: create different component specifically for type= file
 
-const Input = ({ name, type, placeholderText, handleChange }) => {
+const Input = ({ name, label, type, placeholderText, handleChange }) => {
   return (
-    <StyledInput
-      name={name}
-      type={type}
-      placeholder={placeholderText}
-      onChange={handleChange}
-    />
+    <StyledInputContainer>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledInput
+        name={name}
+        type={type}
+        placeholder={placeholderText}
+        onChange={handleChange}
+      />
+    </StyledInputContainer>
   );
 };
 export default Input;
 Input.propTypes = {
+  name: PropTypes.string,
+  label: PropTypes.string,
   type: PropTypes.string.isRequired,
   placeholderText: PropTypes.string,
   handleChange: PropTypes.func,
