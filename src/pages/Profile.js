34d5fd93c3ec4158e918d5f47c236337046a42c2
Linda Okorie <lindaokorie27@gmail.com>
2021-12-09 09:53:21 +0100

    feat: created Profile page layout components and added basic content

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,22 +1,48 @@
-import React from 'react'
-import {CollectionBox, Header, Avatar, UserInfo} from '../styles/ProfileStyles'
+import React from "react";
+import styled from "styled-components";
+import Avatar from "react-avatar";
 
-function Profile(props){
-    return (
-        <div>
-            <Header>
-                <Avatar/>
-                <UserInfo>
-                    <h1>{/*Name of person here*/}</h1>
-                    <button>Edit Profile</button>
-                </UserInfo>
-            </Header>
-            <h2>Collections</h2>
-            <CollectionBox>
-                {/*ImageCollection here*/}
-            </CollectionBox>
-        </div>
-    )
+import defaultPic from "../assets/profile.png";
+import Button from "../components/button/Button";
+import breakpoint from "../common/Breakpoints";
+
+const ProfilePageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and ${breakpoint.device.lg} {
+    flex-direction: row;
+  }
+`;
+
+const CollectionSection = styled.div``;
+
+const ProfileAside = styled.div``;
+
+const UserInfo = styled.div``;
+
+function Profile({ user, handleClick }) {
+  return (
+    <ProfilePageLayout>
+      <ProfileAside>
+        <Avatar
+          name={/*user.username ||*/ "Default"}
+          src={/*user.image ||*/ defaultPic}
+          alt="user profile picture"
+          round={true}
+          //onClick={handleClick}
+        />
+        <UserInfo>
+          <h2>Chris Pratt</h2>
+          <h4>Music loving, Anime loving, Movie acting bloke</h4>
+        </UserInfo>
+        <Button text="Edit Profile" secondary />
+      </ProfileAside>
+      <CollectionSection>
+        <h4>Collection(2)</h4>
+      </CollectionSection>
+    </ProfilePageLayout>
+  );
 }
 
-export default Profile
+export default Profile;
