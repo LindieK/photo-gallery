cf31dfe3f395918a258f759197fe2fafd3fb5178
Linda Okorie <lindaokorie27@gmail.com>
2021-12-19 16:48:35 +0100

    fix: changed component to functional component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -8,18 +8,17 @@ import breakpoint from "../../common/Breakpoints";
 const StyledSearchForm = styled.form`
   width: 14em;
 
-  #search-icon {
-    position: relative;
-    padding: 0.25em;
-    left: -35px;
-    background: none;
-    border: none;
-    cursor: pointer;
-  }
-
   @media only screen and ${breakpoint.device.md} {
     width: 20em;
   }
+`;
+const SearchIcon = styled.button`
+  position: relative;
+  padding: 0.25em;
+  left: -35px;
+  background: none;
+  border: none;
+  cursor: pointer;
 `;
 
 const StyledSearch = styled.input`
@@ -28,28 +27,28 @@ const StyledSearch = styled.input`
   border: 1px solid ${({ theme }) => theme.secondaryText};
   width: 80%;
 `;
-class Search extends Component {
-  render() {
-    const { searchTerm, /*handleSearchTermChange,*/ handleFormSubmit } =
-      this.props;
-    const pattern = /[^\n ]+/;
-    return (
-      <StyledSearchForm onSubmit={handleFormSubmit}>
-        <StyledSearch
-          type="text"
-          placeholder="Search"
-          value={searchTerm}
-          //onChange={handleSearchTermChange}
-          pattern={pattern}
-          title="Must be letters and/or numbers only"
-          required
-        />
-        <button id="search-icon">
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
-      </StyledSearchForm>
-    );
-  }
-}
+
+const Search = ({
+  searchTerm,
+  /*handleSearchTermChange,*/ handleFormSubmit,
+}) => {
+  const pattern = /[^\n ]+/;
+  return (
+    <StyledSearchForm onSubmit={handleFormSubmit}>
+      <StyledSearch
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        //onChange={handleSearchTermChange}
+        pattern={pattern}
+        title="Must be letters and/or numbers only"
+        required
+      />
+      <SearchIcon>
+        <FontAwesomeIcon icon={faSearch} />
+      </SearchIcon>
+    </StyledSearchForm>
+  );
+};
 
 export default Search;
