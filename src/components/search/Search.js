2266f48be8c052491d22a373ee8eb3170068825b
Linda Okorie <lindaokorie27@gmail.com>
2021-12-19 16:43:47 +0100

    fix: created styled components that will form basis of search bar styling and postioned search bar in navigation

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+import breakpoint from "../../common/Breakpoints";
+
+const StyledSearchForm = styled.form`
+  width: 14em;
+
+  #search-icon {
+    position: relative;
+    padding: 0.25em;
+    left: -35px;
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+
+  @media only screen and ${breakpoint.device.md} {
+    width: 20em;
+  }
+`;
+
+const StyledSearch = styled.input`
+  padding: 0.5em 1em;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.secondaryText};
+  width: 80%;
+`;
 class Search extends Component {
   render() {
     const { searchTerm, /*handleSearchTermChange,*/ handleFormSubmit } =
       this.props;
     const pattern = /[^\n ]+/;
     return (
-      <form onSubmit={handleFormSubmit}>
-        <input
+      <StyledSearchForm onSubmit={handleFormSubmit}>
+        <StyledSearch
           type="text"
           placeholder="Search"
           value={searchTerm}
@@ -16,7 +44,10 @@ class Search extends Component {
           title="Must be letters and/or numbers only"
           required
         />
-      </form>
+        <button id="search-icon">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </StyledSearchForm>
     );
   }
 }
