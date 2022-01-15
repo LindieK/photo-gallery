157f261df9b283072b38e58b27f24d22ffa85202
Linda Okorie <lindaokorie27@gmail.com>
2022-01-15 13:18:13 +0100

    fix: added common modal styling and general styling

--- src/common/CommonStyles.js
+++ src/common/CommonStyles.js
@@ -1,6 +1,39 @@
 import styled from "styled-components";
 import breakpoint from "./Breakpoints";
 
+/**
+ * General
+ */
+export const SecondaryLink = styled.a`
+  background-color: transparent;
+  font-size: 1em;
+  color: #767676;
+  padding: 0.5em 0.6em;
+  margin: 0 0.25em;
+  border-radius: 5px;
+  border: 1.5px solid #d1d1d1;
+  cursor: pointer;
+
+  & > span {
+    padding-left: 0.5em;
+  }
+  &:hover {
+    color: #000000;
+    border: 2px solid #000000;
+  }
+
+  @media only screen and (${breakpoint.device.sm}) {
+    font-size: 0.8em;
+
+    & > span {
+      display: none;
+    }
+  }
+`; //Change colour codes
+
+/**
+ * Auth Pages
+ */
 export const AuthenticationPageContainer = styled.div`
   width: 80%;
   margin: 0 auto;
@@ -75,4 +108,26 @@ export const LoginText = styled(SupportText)`
   @media only screen and ${breakpoint.device.xlg} {
     font-size: 1.76em;
   }
+`;
+
+/**
+ * Modal Components
+ */
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  backdrop-filter: blur(5px);
+`;
+
+export const ModalWrapper = styled.div`
+  position: relative;
+  background-color: rgb(255, 255, 255);
+  border-radius: 10px;
 `;
