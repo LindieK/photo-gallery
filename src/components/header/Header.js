97716f7f9ea01018cd4fce78c08139bac819d34f
Linda Okorie <lindaokorie27@gmail.com>
2022-01-15 13:29:49 +0100

    fix: removed nav from header and deleted app css file

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,11 @@
 import React from "react";
 
 import Categories from "../categories/Categories";
-import Nav from "../nav/Nav";
 
 const Header = (props) => {
-  const {
-    setActiveTab,
-    isTabActive,
-    searchTerm,
-    //handleSearchTermChange,
-    handleFormSubmit,
-  } = props;
+  const { setActiveTab, isTabActive } = props;
   return (
     <header>
-      <Nav
-        searchTerm={searchTerm}
-        //handleSearchTermChange={handleSearchTermChange}
-        handleFormSubmit={handleFormSubmit}
-      />
       <Categories setActiveTab={setActiveTab} isTabActive={isTabActive} />
     </header>
   );
