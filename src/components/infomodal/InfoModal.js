00aa535425f58a7ccd930cfd81a33ca1f3c6bf98
Linda Okorie <lindaokorie27@gmail.com>
2022-01-15 13:17:21 +0100

    fix: moved info modal css to styled components

--- src/components/infomodal/InfoModal.js
+++ src/components/infomodal/InfoModal.js
@@ -1,9 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { InfoModalOverlay, InfoWrapper } from "../../styles/ModalStyles";
-import "./InfoModal.scss";
+
+import breakpoint from "../../common/Breakpoints";
+import { ModalOverlay, ModalWrapper } from "../../common/CommonStyles";
+
+const InfoModalOverlay = styled(ModalOverlay)`
+  z-index: 20;
+`;
+const InfoModalWrapper = styled(ModalWrapper)`
+  width 40%;
+  padding: 1em;
+
+  #modalTitle{
+    h2{
+      margin: .8em 0 0;
+    }
+    .text{
+      font-size: .8em;
+      color: #767676;
+      display: block;
+      margin-bottom: .2em;
+    }
+  }
+
+  #downloads{
+    padding: 1em 0;
+
+    .titleValue{
+      font-size: 1.25em;
+    }
+    span{
+      font-size: .8em;
+      margin-left: .25em;
+    }
+
+    .icon{
+      margin-right: .25em;
+      font-size: .8em;
+    }
+  }
+
+  hr{
+    color: #d1d1d1;
+  }
+
+  #cameraDetails{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    div{
+      padding: .5em;
+    }
+  }
+
+  @media only screen and (${breakpoint.device.sm}){
+    width: 80%;
+  }
+`;
 
 const InfoModal = (props) => {
   let resolution = `${props.imageInfo.width} x ${props.imageInfo.height}`;
@@ -11,7 +67,7 @@ const InfoModal = (props) => {
 
   return ReactDOM.createPortal(
     <InfoModalOverlay onClick={props.onClose}>
-      <InfoWrapper>
+      <InfoModalWrapper>
         <div id="modalTitle">
           <h2>Info</h2>
           <span className="text">Published on {datePubllished}</span>
@@ -60,7 +116,7 @@ const InfoModal = (props) => {
             </span>
           </div>
         </div>
-      </InfoWrapper>
+      </InfoModalWrapper>
     </InfoModalOverlay>,
     document.getElementById("info-root")
   );
