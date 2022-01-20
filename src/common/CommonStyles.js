d95568ffa33ad238549a394f4b790f47917f63fe
Linda Okorie <lindaokorie27@gmail.com>
2022-01-20 21:52:52 +0100

    fix: moved styles from grid styles file to where they should be

--- src/common/CommonStyles.js
+++ src/common/CommonStyles.js
@@ -131,3 +131,29 @@ export const ModalWrapper = styled.div`
   background-color: rgb(255, 255, 255);
   border-radius: 10px;
 `;
+/**
+ * Empty States
+ */
+export const ResultsMsg = styled.div`
+  font-size: 1.25em;
+  color: #c4c4c4;
+`;
+export const ResultsIcon = styled.img.attrs((props) => ({
+  src: props.src,
+  alt: props.alt,
+}))`
+  width: 10vw;
+  height: 10vw;
+`;
+export const SearchEmptyState = styled.div`
+  position: relative;
+  top: 14em;
+  width: 75vw;
+  height: 50vh;
+  margin: 0 auto;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
--- src/components/layout/Grid.js
+++ src/components/layout/Grid.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
 import {
-  GridBox,
   SearchEmptyState,
   ResultsIcon,
   ResultsMsg,
-} from "../../styles/GridBoxStyles";
+} from "../../common/CommonStyles";
 import LazyImageCard from "../lazyimage/LazyImageCard";
 import icon from "../../assets/search-results.svg";
 
+const GridBox = styled.div`
+  position: relative;
+  top: 14em;
+  width: 75vw;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1em;
+  padding: 1em;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 767px) {
+    grid-template-columns: 1fr;
+    top: 10em;
+    width: 90vw;
+  }
+  @media only screen and (min-width: 768px) and (max-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+    top: 12em;
+  }
+`;
 class Grid extends Component {
   render() {
     const { photos, query, displayModal } = this.props;
