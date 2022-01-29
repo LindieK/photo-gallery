23eb800e173fcd27761bf7a9b3cac3e9ffbf709b
Linda Okorie <lindaokorie27@gmail.com>
2022-01-29 17:09:08 +0100

    feat: created hero section that will display when a user is not signed in

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,5 +1,41 @@
-import React from "react";
+import styled from "styled-components";
+
+import breakpoint from "../../common/Breakpoints";
+import HeroPic from "../../assets/mountains-kai-oberhauser.jpg";
+import Button from "../button/Button";
+
+const StyledHero = styled.div`
+  background-image: url(${HeroPic});
+  background-position: center;
+  padding: 2em;
+
+  @media only screen and ${breakpoint.device.lg} {
+    height: 50vh;
+    padding: 3em;
+  }
+`;
+
+const HeroTextSection = styled.div`
+  text-align: center;
+
+  @media only screen and ${breakpoint.device.lg} {
+    h1 {
+      font-size: 3.25em;
+    }
+  }
+`;
 
 export default function Hero() {
-  return <div></div>;
+  return (
+    <StyledHero>
+      <HeroTextSection>
+        <h1> Discover the art of photography</h1>
+        <p>
+          Search for and collect great stock photos shared by creators all in
+          one place.
+        </p>
+        <Button text="Get Started" size="1em" />
+      </HeroTextSection>
+    </StyledHero>
+  );
 }
