d0f91f3f69070db37926f8493afe294369515e40
Linda Okorie <lindaokorie27@gmail.com>
2022-02-11 13:51:43 +0100

    fix: restructured hero to fit new design

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,21 +2,45 @@ import styled from "styled-components";
 
 import breakpoint from "../../common/Breakpoints";
 import HeroPic from "../../assets/mountains-kai-oberhauser.jpg";
-import Button from "../button/Button";
+import Search from "../search/Search";
 
 const StyledHero = styled.div`
   background-image: url(${HeroPic});
   background-position: center;
   padding: 2em;
+  height: 65vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  @media only screen and ${breakpoint.device.md} {
+    justify-content: center;
+  }
   @media only screen and ${breakpoint.device.lg} {
-    height: 50vh;
+    height: 55vh;
     padding: 3em;
   }
 `;
 
 const HeroTextSection = styled.div`
   text-align: center;
+
+  h1 {
+    font-size: 1.5em;
+  }
+  p {
+    font-size: 0.89em;
+  }
+
+  @media only screen and ${breakpoint.device.md} {
+    h1 {
+      font-size: 2em;
+    }
+    p {
+      font-size: 1em;
+    }
+  }
 
   @media only screen and ${breakpoint.device.lg} {
     h1 {
@@ -25,7 +49,9 @@ const HeroTextSection = styled.div`
   }
 `;
 
-export default function Hero() {
+const Hero = (props) => {
+  const { searchTerm, /*handleSearchTermChange,*/ handleFormSubmit } = props;
+
   return (
     <StyledHero>
       <HeroTextSection>
@@ -34,8 +60,13 @@ export default function Hero() {
           Search for and collect great stock photos shared by creators all in
           one place.
         </p>
-        <Button text="Get Started" size="1em" />
       </HeroTextSection>
+      <Search
+        searchTerm={searchTerm}
+        //handleSearchTermChange={handleSearchTermChange}
+        handleFormSubmit={handleFormSubmit}
+      />
     </StyledHero>
   );
-}
+};
+export default Hero;
