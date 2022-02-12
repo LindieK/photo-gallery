b9baa79200be65df21ab98517ae1ea2c9e172791
Linda Okorie <lindaokorie27@gmail.com>
2022-02-12 10:13:58 +0100

    fix: arranged styling in a mobile first approach

--- src/components/layout/Grid.js
+++ src/components/layout/Grid.js
@@ -1,71 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 
-import {
-  SearchEmptyState,
-  ResultsIcon,
-  ResultsMsg,
-} from "../../common/CommonStyles";
+import breakpoint from "../../common/Breakpoints";
 import LazyImageCard from "../lazyimage/LazyImageCard";
-import icon from "../../assets/search-results.svg";
 
 const GridBox = styled.div`
-  position: relative;
-  top: 14em;
-  width: 75vw;
+  width: 90vw;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr;
   gap: 1em;
   padding: 1em;
   margin: 0 auto;
 
-  @media only screen and (max-width: 767px) {
-    grid-template-columns: 1fr;
-    top: 10em;
-    width: 90vw;
-  }
-  @media only screen and (min-width: 768px) and (max-width: 992px) {
+  @media only screen and ${breakpoint.device.md} {
     grid-template-columns: 1fr 1fr;
-    top: 12em;
+  }
+  @media only screen and ${breakpoint.device.lg} {
+    grid-template-columns: 1fr 1fr 1fr;
+    top: 10em;
+    width: 75vw;
   }
 `;
-class Grid extends Component {
-  render() {
-    const { photos, query, displayModal } = this.props;
-    return (
-      <>
-        {photos.length === 0 ? (
-          <SearchEmptyState>
-            <ResultsIcon src={icon} alt="Empty Search State Icon" />
-            {query ? (
-              <ResultsMsg>
-                <p className="error-msg">No results for {query}</p>
-                <p className="error-msg">Try a different search term</p>
-              </ResultsMsg>
-            ) : (
-              <ResultsMsg>
-                <p className="error-msg">
-                  Oh my something went wrong. There are no pictures to show you.
-                </p>
-                <p className="error-msg">Try refreshing the page</p>
-              </ResultsMsg>
-            )}
-          </SearchEmptyState>
-        ) : (
-          <GridBox>
-            {photos.map((photo) => (
-              <LazyImageCard
-                key={photo.id}
-                image={photo.urls}
-                alt={photo.alt_description}
-                onImageClick={() => displayModal(photo)}
-              />
-            ))}
-          </GridBox>
-        )}
-      </>
-    );
-  }
-}
+const Grid = (props) => {
+  const { photos, displayModal } = props;
+  return (
+    <>
+      {photos.length > 0 && (
+        <GridBox>
+          {photos.map((photo) => (
+            <LazyImageCard
+              key={photo.id}
+              image={photo.urls}
+              alt={photo.alt_description}
+              onImageClick={() => displayModal(photo)}
+            />
+          ))}
+        </GridBox>
+      )}
+    </>
+  );
+};
 
 export default Grid;
