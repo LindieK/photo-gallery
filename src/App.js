a48e43eb47b961bbd8999b5b44c53b9abeb84bcd
Linda Okorie <lindaokorie27@gmail.com>
2022-02-20 13:16:29 +0100

    fix: imported AuthProvider correctly from

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import AppThemeProvider from "./context/ThemeContext";
-import { AuthProvider } from "./context/AuthContext";
+import AuthProvider from "./context/AuthContext";
 import Router from "./routes/index";
 import GlobalStyle from "./common/GlobalStyles";
 
