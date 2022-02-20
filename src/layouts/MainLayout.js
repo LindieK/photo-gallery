63a88b4faa7d98bd90bca9882dfe862cdb9590df
Linda Okorie <lindaokorie27@gmail.com>
2022-02-20 13:17:56 +0100

    feat: added nav based on user state

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,18 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { Outlet } from "react-router";
 
-//import DefaultNav from "../components/nav/DefaultNav";
+import DefaultNav from "../components/nav/DefaultNav";
 import HeroNav from "../components/nav/HeroNav";
+import { useAuth } from "../context/AuthContext";
 
 const StyledMainLayout = styled.div`
   position: relative;
 `;
 
 const MainLayout = () => {
+  const { currentUser } = useAuth();
   return (
     <StyledMainLayout>
-      <HeroNav />
-      {/*<DefaultNav />*/}
+      {currentUser ? <DefaultNav /> : <HeroNav />}
       {/* searchTerm={searchTerm} handleSearchTermChange={handleSearchTermChange}  handleFormSubmit={handleFormSubmit} */}
       <Outlet />
     </StyledMainLayout>
