cb09e2b8a64b434ebee08a436511ccc48e5c0af9
Linda Okorie <lindaokorie27@gmail.com>
2022-02-21 13:28:45 +0100

    fix: started adding functionality to sign up form
    I will add validation and error handling next

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Button from "../components/button/Button";
 import Input from "../components/input/Input";
+import { useAuth } from "../context/AuthContext";
 import {
   LogoText,
   AuthenticationForm,
@@ -12,6 +13,67 @@ import {
 } from "../common/CommonStyles";
 
 const SignUp = () => {
+  const { signup } = useAuth();
+  const [passwordStrength, setPasswordStrength] = useState("");
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const validate = (name, value) => {
+    let isValid;
+    switch (name) {
+      case "username":
+        let userNameRegEx = "";
+        isValid = userNameRegEx.test(value);
+        break;
+      case "email":
+        let emailRegEx = "";
+        isValid = emailRegEx.test(value);
+        break;
+      case "password":
+        let passwordRegEx = new RegExp(
+          "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})"
+        );
+        isValid = passwordRegEx.test(value);
+        break;
+      default:
+        break;
+    }
+
+    return isValid;
+  };
+
+  const validatePassword = (value) => {
+    const strongRegEx = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})"
+    );
+    const mediumRegex = new RegExp(
+      "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
+    );
+
+    if (strongRegEx.test(value)) {
+      setPasswordStrength("green");
+    } else if (mediumRegex.test(value)) {
+      setPasswordStrength("orange");
+    }
+  };
+  const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { username, email, password } = userInfo;
+    await signup(username, email, password);
+  };
+
   return (
     <AuthenticationPageContainer>
       <LogoText>PG</LogoText>
@@ -28,20 +90,26 @@ const SignUp = () => {
           label="Username*"
           type="text"
           placeholderText="Username"
+          value={userInfo.username}
+          handleChange={handleChange}
         />
         <Input
           name="email"
           label="E-mail*"
           type="email"
           placeholderText="E-mail"
+          value={userInfo.email}
+          handleChange={handleChange}
         />
         <Input
           name="password"
           label="Password*"
           type="password"
           placeholderText="Password"
+          value={userInfo.password}
+          handleChange={handleChange}
         />
-        <Button text="Sign Up" />
+        <Button text="Sign Up" handleClick={handleSubmit} />
 
         <LoginText>
           Already have an account? <Link to="/login">Log In</Link>
