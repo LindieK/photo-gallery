3cc8acbda1bc92dd7fe16b875e871acd97cef472
Linda Okorie <lindaokorie27@gmail.com>
2022-06-30 14:18:56 +0100

    fix: configured image download as per guidelines

--- src/Api.js
+++ src/Api.js
@@ -1,26 +1,30 @@
 import axios from "axios";
 
-const clientID = "HpJbqQZaynDRjC2TKwQKEhJ2tVjKU9eSR23ICo8_b1I";
 const numberOfPhotos = 30;
 let url;
 
 export const getSearchResults = (searchValue) => {
   url = searchValue
-    ? `https://api.unsplash.com/search/photos?client_id=${clientID}&per_page=${numberOfPhotos}&query=${searchValue}`
+    ? `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_API_KEY}&per_page=${numberOfPhotos}&query=${searchValue}`
     : ``;
   return axios.get(url).then((response) => response);
 };
 
 export const getCategoryPics = (Category) => {
   url = Category
-    ? `https://api.unsplash.com/photos/random/?client_id=${clientID}&count=${numberOfPhotos}&query=${Category}`
+    ? `https://api.unsplash.com/photos/random/?client_id=${process.env.REACT_APP_API_KEY}&count=${numberOfPhotos}&query=${Category}`
     : ``;
   return axios.get(url).then((response) => response);
 };
 
 export const getInitialPics = async () => {
-  url = `https://api.unsplash.com/photos/random/?client_id=${clientID}&count=${numberOfPhotos}`;
+  url = `https://api.unsplash.com/photos/random/?client_id=${process.env.REACT_APP_API_KEY}&count=${numberOfPhotos}`;
   return axios.get(url).then((response) => {
     return response;
   });
+};
+
+export const trackDownload = async (downloadLocation) => {
+  url = `${downloadLocation}&client_id=${process.env.REACT_APP_API_KEY}`;
+  return axios.get(url).then((response) => response);
 };
