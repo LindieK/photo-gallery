dc29b02129024f8fb326e3de782149cd33c80ed8
Linda Okorie <lindaokorie27@gmail.com>
2022-07-18 13:10:07 +0100

    fix: corrected package referencing

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 import breakpoint from "../common/Breakpoints";
 import coverImage from "../assets/Sunset_Waterfall_Landscape_Illustration.webp";
