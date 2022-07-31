d0d59f3c996981ccbf9188d165bf94efffc0ba70
Linda Okorie <lindaokorie27@gmail.com>
2022-07-31 10:37:07 +0100

    fix: corrected ordering of imports

--- src/components/button/Button.spec.js
+++ src/components/button/Button.spec.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Button from "./Button";
 import { render, screen, fireEvent } from "@testing-library/react";
+
+import Button from "./Button";
 
 describe("Button component", () => {
   const props = {
