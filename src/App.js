9621199d7bb39c7f1ca46bd31d0a0a896cad8b37
Linda Okorie <lindaokorie27@gmail.com>
2022-08-09 13:54:04 +0100

    fix: removed Styled container

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import AppThemeProvider from "./context/ThemeContext";
 import AuthProvider from "./context/AuthContext";
 import Router from "./routes/index";
 import GlobalStyle from "./common/GlobalStyles";
-import { StyledToastContainer } from "./common/CommonStyles";
+//import { StyledToastContainer } from "./common/CommonStyles";
 
 const App = () => {
   return (
     <AppThemeProvider>
       <GlobalStyle />
       <AuthProvider>
-        <StyledToastContainer position="top-center" />
+        {/* <StyledToastContainer position="top-center" /> */}
         <Router />
       </AuthProvider>
     </AppThemeProvider>
